a4deb987d4f0654fe9c8fc607bff2fb236aa92e9
daichi-asami <daichi025@icloud.com>
2021-02-03 10:00:52 +0000

    aaa

--- db/migrate/20210127101408_create_questions.rb
+++ db/migrate/20210127101408_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-      t.references :q
+      t.references :content
       t.string :question 
       t.timestamps null: false
     end
